5a29fc6a0e5fc35611f4310e8977458154e320c7
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-11-13 01:22:32 +0200

    final fixes

--- miraplay_test_client/src/pages/GamesPage/GamesPage.jsx
+++ miraplay_test_client/src/pages/GamesPage/GamesPage.jsx
@@ -1,7 +1,5 @@
 import { CardList } from '../../components/CardList';
 import { Header } from '../../components/Header';
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import './GamesPage.scss';
 
 export const GamesPage = () => {
